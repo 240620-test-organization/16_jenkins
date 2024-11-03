b58aaa4a864602f74cbb7a9eae114cb28f2530c4
blimu <sheep.ohgiraffers@gmail.com>
2024-11-03 17:51:11 +0900

    test-pipeline

--- src/main/java/com/ohgiraffers/jenkins/controller/CalculatorController.java
+++ src/main/java/com/ohgiraffers/jenkins/controller/CalculatorController.java
@@ -17,7 +17,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "v2";
+        return "test-pipeline";
     }
 
     @GetMapping("/plus")
