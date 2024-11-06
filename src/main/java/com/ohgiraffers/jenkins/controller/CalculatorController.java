d3bf08a606ef1c629c0c9be8050d47e5397ef83a
blimu <sheep.ohgiraffers@gmail.com>
2024-11-06 15:06:47 +0900

    commit

--- src/main/java/com/ohgiraffers/jenkins/controller/CalculatorController.java
+++ src/main/java/com/ohgiraffers/jenkins/controller/CalculatorController.java
@@ -17,7 +17,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "ArgoCD test again";
+        return "ArgoCD test";
     }
 
     @GetMapping("/plus")
